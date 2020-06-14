a7c7479d456b7f39778117a714e798c7f3f1f5ba
Daniel Ciulinaru <66647414+danielciulinaru@users.noreply.github.com>
2020-06-13 22:49:54 -0400

    final form of the subclassed dictionary

--- orautil/core/helpers.py
+++ orautil/core/helpers.py
@@ -1,10 +1,19 @@
 class SpecialDict(dict):
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+        super(SpecialDict, self).__init__(*args, **kwargs)
+
     def __setitem__(self, key, value):
-        if key in super(SpecialDict, self).keys():
-            values = super(SpecialDict, self).pop(key)
+        if key in dict.keys(self):
+            values = dict.pop(self, key)
             values = set(values)
             values.add(value)
-        elif key not in super(SpecialDict, self).keys():
+        elif key not in dict.keys(self):
             values = set()
             values.add(value)
-        super(SpecialDict, self).__setitem__(key, values)
+        values = list(values)
+        values.sort()
+        dict.__setitem__(self, key, values)
+
+    def __getitem__(self, key):
+        return dict.__getitem__(self, key)
